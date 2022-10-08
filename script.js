be295dbd0e63badfd0dfe6e25a154540d58884f2
Victor <fisheve@mx.lakeforest.edu>
2022-10-08 05:50:46 -0500

    Recordings Section Added

---
+++ script.js
@@ -0,0 +1,9 @@
+// https://stackoverflow.com/questions/19790506/multiple-audio-html-auto-stop-other-when-current-is-playing-with-javascript
+document.addEventListener('play', function(e){
+  var audios = document.getElementsByTagName('audio');
+  for(var i = 0, len = audios.length; i < len;i++){
+      if(audios[i] != e.target){
+          audios[i].pause();
+      }
+  }
+}, true);
