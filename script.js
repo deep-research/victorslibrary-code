4fb3980f219587e80a29ac65dc5b2c332e636cb9
Victor <fisheve@mx.lakeforest.edu>
2022-10-16 17:56:57 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 // https://stackoverflow.com/questions/19790506/multiple-audio-html-auto-stop-other-when-current-is-playing-with-javascript
 document.addEventListener('play', function(e){
-  var audios = document.getElementsByTagName('audio');
-  for(var i = 0, len = audios.length; i < len;i++){
-      if(audios[i] != e.target){
-          audios[i].pause();
+  // var audios = document.getElementsByTagName('audio');
+  var media = document.querySelectorAll('audio,video')
+  for(var i = 0, len = media.length; i < len;i++){
+      if(media[i] != e.target){
+        media[i].pause();
       }
   }
 }, true);
